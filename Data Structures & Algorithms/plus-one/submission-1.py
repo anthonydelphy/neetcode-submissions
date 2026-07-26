c6856707da8253af26cebafc6e0e9bb5ab20b0ed
anthonydelphy <73381837+anthonydelphy@users.noreply.github.com>
2026-07-26 11:54:26 -0500

    Add: plus-one - submission-1

---
+++ Data Structures & Algorithms/plus-one/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        s = ''
+        for x in digits:
+            s += str(x)
+        
+        s = str(int(s)+1)
+        print(s)
+        result = []
+        for x in s:
+            result.append(int(x))
+        return result
