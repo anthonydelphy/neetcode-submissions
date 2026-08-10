5dcd66496d1d319c8abe9cb06485afcb773aa3da
anthonydelphy <73381837+anthonydelphy@users.noreply.github.com>
2026-08-10 00:40:37 -0500

    Add: anagram-groups - submission-12

---
+++ Data Structures & Algorithms/anagram-groups/submission-12.py
@@ -0,0 +1,16 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # Create a lst of dictionaries
+        # I want a list of dictionaries, with the dictionary keys storing the set and the value being the list of anagrams
+        
+        #Create a list
+        lst = {}
+        result = []
+        for i in strs:
+            srt = "".join(sorted(i))
+            if srt in lst:
+                lst[srt].append(i)
+                continue
+            lst[srt] = [i]
+            
+        return list(lst.values())
