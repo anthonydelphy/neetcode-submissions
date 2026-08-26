715ac5a1ec013233c14a7d9b6e3693996dc2f7d9
anthonydelphy <73381837+anthonydelphy@users.noreply.github.com>
2026-08-26 10:35:20 -0500

    Add: eating-bananas - submission-9

---
+++ Data Structures & Algorithms/eating-bananas/submission-9.py
@@ -0,0 +1,21 @@
+class Solution:
+    def eatingBananas(self,piles, speed) -> int:
+        result = 0
+        for i in piles:
+            result += -(-i//speed)
+        return result
+    
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l=k = 1
+        r = max(piles)
+        while l <= r:
+            m = l+ ((r-l)//2)
+            hoursTook = self.eatingBananas(piles,m)
+            if hoursTook > h:
+                l = m+1
+            elif hoursTook <= h:
+                k = m
+                r = m-1
+
+        return k
+
