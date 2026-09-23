b01e3389a64711ab2bbc897154e68d92de8da0ee
anthonydelphy <73381837+anthonydelphy@users.noreply.github.com>
2026-09-23 10:21:18 -0500

    Add: implement-prefix-tree - submission-12

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-12.py
@@ -0,0 +1,44 @@
+class TreeNode:
+    def __init__(self):
+        self.endOfWord = False
+        self.children = {}
+
+class PrefixTree:
+
+    def __init__(self):
+        self.root = TreeNode()
+
+    def insert(self, word: str) -> None:
+        temp = self.root
+        for i in word:
+            if i not in temp.children:
+                temp.children[i] = TreeNode()
+            temp = temp.children[i]
+        temp.endOfWord = True
+
+    def search(self, word: str) -> bool:
+        temp = self.root
+        if len(temp.children) == 0:
+            return False
+
+        for i in word:
+            if i not in temp.children:
+                return False
+            temp = temp.children[i]
+
+        return temp.endOfWord
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        temp = self.root
+        if len(temp.children) == 0:
+            return False
+
+        for i in prefix:
+            if i not in temp.children:
+                return False
+            temp = temp.children[i]
+        return True
+        
+
+        
